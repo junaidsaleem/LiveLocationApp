aa27dc978e9632cc2e06fa4328668b98816998e1
Junaid Saleem <junaidsaleem.ctn@gmail.com>
2023-07-07 02:47:46 +0500

    Pod update

--- ios/LiveLocationApp/AppDelegate.mm
+++ ios/LiveLocationApp/AppDelegate.mm
@@ -1,11 +1,16 @@
 #import "AppDelegate.h"
-
+#import <Firebase.h>
+#import <GoogleMaps/GoogleMaps.h>
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
+    
   self.moduleName = @"LiveLocationApp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
